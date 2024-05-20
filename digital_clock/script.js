82204f77dd81a2eaf91258b09827faba18efcf32
Saikat <saikatchakravortyofficial@gmail.com>
2024-05-20 11:59:01 +0530

    Digital Clock

---
+++ digital_clock/script.js
@@ -0,0 +1,7 @@
+const clock = document.getElementById('clock');
+// const clock = document.querySelector('#clock')
+
+setInterval(function () {
+  let date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
+});
