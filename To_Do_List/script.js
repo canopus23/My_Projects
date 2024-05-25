c542fd4cbbb992f3bf857b78d5ab1deebc90086f
Saikat <saikatchakravortyofficial@gmail.com>
2024-05-26 00:40:13 +0530

    Updated

---
+++ To_Do_List/script.js
@@ -0,0 +1,28 @@
+
+const input = document.getElementById('input');
+const listContainer = document.getElementById('list-container');
+const button = document.querySelector('button');
+
+
+button.addEventListener('click', 
+function textInput(){
+    if(input.value === ''){
+        alert("Please Write Something")
+    } else{
+        let li = document.createElement('li');
+        li.innerHTML = input.value;
+        listContainer.appendChild(li)
+        input.value = "";
+
+
+        li.addEventListener('click', () => {
+            li.style.textDecoration = li.style.textDecoration === 'line-through' ? 'none' : 'line-through';
+        });
+
+
+        li.addEventListener('dblclick', () => {
+            listContainer.removeChild(li)
+        })
+    }
+    
+})
