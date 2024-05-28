dfccf1cd1fd5a401dbd986beae5e987d3bccf1c0
Saikat <saikatchakravortyofficial@gmail.com>
2024-05-28 14:48:52 +0530

    Updated

--- Jokes/script.js
+++ Jokes/script.js
@@ -2,7 +2,6 @@ const joke_text = document.querySelector('.joke-text');
 
 const joke_btn = document.querySelector('.new-joke-btn');
 
-const tweet_btn = document.querySelector('.tweet-btn');
 
 
 joke_btn.addEventListener('click', getjoke);
