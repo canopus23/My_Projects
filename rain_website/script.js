600db3a461c9115987b6a9fb181a7e9c37e2671a
Saikat <saikatchakravortyofficial@gmail.com>
2024-07-16 15:41:58 +0530

    2

--- rain_website/script.js
+++ rain_website/script.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const rainContainer = document.querySelector('.rain-container');
+    const quoteContainer = document.querySelector('.quote');
+    const toggleRainButton = document.querySelector('.toggle-rain');
+    let rainInterval;
+    
+    const quotes = [
+        "The best thing one can do when it's raining is to let it rain. - Henry Wadsworth Longfellow",
+        "Some people walk in the rain, others just get wet. - Roger Miller",
+        "Rain is grace; rain is the sky descending to the earth; without rain, there would be no life. - John Updike"
+    ];
 
     function createRaindrop() {
         const raindrop = document.createElement('div');
@@ -9,11 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
         raindrop.style.opacity = Math.random();
         rainContainer.appendChild(raindrop);
 
-        // Remove the raindrop after it falls out of view
         setTimeout(() => {
             raindrop.remove();
         }, 1000);
     }
 
-    setInterval(createRaindrop, 50);
-});
+    function startRain() {
+        rainInter
